68477f2b489a735d9829b151a192c9650017809f
soumya <soumyasundardas2@gmail.com>
2025-12-06 15:31:43 +0530

    practice:DSA started arraylist done

---
+++ src/dsa/CollectionFramework/arrayList/prac1test.java
@@ -0,0 +1,36 @@
+package dsa.CollectionFramework.arrayList;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class prac1test {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        //add elements;
+        list.add(0);
+        list.add(1);
+        list.add(3);
+        System.out.println(list);
+        //to get element using index
+        System.out.println(list.get(2));
+        //to add element using index in between or anywhere
+        list.add(1,2);
+        System.out.println(list);
+        //to set element (to replace any element)
+        list.set(0,1);
+        System.out.println(list);
+
+        //delete element
+        list.remove(2);
+        System.out.println(list);
+
+        //to see the size of list
+        System.out.println(list.size());
+
+        //sorting
+        list.add(0,7);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+
+    }
+}
