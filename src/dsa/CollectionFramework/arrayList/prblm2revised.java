8a50743e2267fac270027251211a838d7cdec7c9
soumya <soumyasundardas2@gmail.com>
2025-12-10 12:29:02 +0530

    practice:arraylist problem 2 resolved again in more advanced way

--- src/dsa/CollectionFramework/arrayList/prblm2revised.java
+++ src/dsa/CollectionFramework/arrayList/prblm2revised.java
@@ -1,4 +1,5 @@
 package dsa.CollectionFramework.arrayList;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,31 +17,54 @@ public class prblm2revised {
         }
         System.out.println("The List : " + list);
         System.out.println();
-
-        int max = list.get(0);
-        for (int i = 1; i < list.size(); i++) {
-            if (list.get(i) > max) {
-                max = list.get(i);
+        int max=list.get(0);
+        for(int j=1;j<list.size();j++){
+            if(list.get(j)>max){
+                max=list.get(j);
             }
         }
-        System.out.println("Max value int list: " + max);
-        Integer max2 = null;
+        System.out.println("Maximum Value in list: "+max);
+        //Finding 2nd maximum:
+        Integer max2= null;
+        for (int k=0;k< list.size();k++){
+            int val= list.get(k);
+            if (val==max){
+                continue;
+            }
+            if (max2==null || val>max2){
+                max2=val;
 
-        for (int j = 0; j < list.size(); j++) {
-            int val = list.get(j);
-            if (val != max) {     // must exclude max
-                if (max2 == null || val > max2) {
-                    max2 = val;
-                }
             }
         }
-
-        System.out.println("Max value in list: " + max);
-
-        if (max2 == null) {
-            System.out.println("Second max does NOT exist.");
-        } else {
-            System.out.println("2nd Max value in list: " + max2);
+        if (max2==null){
+            System.out.println("2nd max not exist..");
         }
+        else{
+            System.out.println("2nd max: "+max2);
+        }
+        //Finding 3rd max Value
+        Integer max3=null;
+        for (int l=0;l<list.size();l++){
+            int num=list.get(l);
+            if(num==max || num==max2){
+                continue;
+            }
+            if (max3==null || num>max3){
+                max3=num;
+            }
+        }
+        if (max3==null){
+            System.out.println("3rd maximum no. doesnot exist..");
+        }
+        else {
+            System.out.println("3rd maimum no. : "+max3);
+        }
+
+
+
+
+
+
+
     }
 }
