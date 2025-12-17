e90d25b8f8b0986a413983df7a40caef4144d46c
soumya <soumyasundardas2@gmail.com>
2025-12-17 17:08:03 +0530

    practice: array problm5 solved

---
+++ src/arrays/problm5.java
@@ -0,0 +1,28 @@
+package arrays;
+
+public class problm5 {
+    public static void main(String[] args) {
+        int[] arr= {9,3,7,1,5};
+        boolean assending=true;
+        boolean desending=true;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]<arr[i+1]){
+                assending=false;
+            }
+            if (arr[i]>arr[i+1]){
+                desending=false;
+            }
+
+        }
+        if (assending){
+            System.out.println("Assending order");
+        } else if (desending) {
+            System.out.println("Decending order");
+
+        }
+        else {
+            System.out.println("no order");
+        }
+
+    }
+}
