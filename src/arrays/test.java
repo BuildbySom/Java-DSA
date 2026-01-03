c8f5526b6ee218dfd7cd068a231fb24cff8b77ff
soumya <soumyasundardas2@gmail.com>
2026-01-03 20:21:15 +0530

    practice:a problem proccessing

--- src/arrays/test.java
+++ src/arrays/test.java
@@ -6,18 +6,31 @@ public class test {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int[] marks = new int[5];
+        int[] freg= new int[1000];
         int user_input;
+        System.out.println("Enter "+5+" Marks: ");
+
         for(int i=0;i<5;i++){
-            System.out.println("Enter "+(i+1)+" Marks: ");
-            user_input=sc.nextInt();
-            marks[i]=user_input;
+            marks[i]=sc.nextInt();
+
 
         }
         for(int i=0;i<5;i++){
-            System.out.println((i+1)+" marks: "+marks[i]);
+            System.out.print(" "+marks[i]);
 
         }
         System.out.println(marks.length);
+        for(int i=0;i<5;i++){
+            freg[marks[i]]++;
+
+        }
+        System.out.println("FreQuency: ");
+        for(int i=0;i<10;i++){
+            System.out.print(" "+freg[i]);
+
+        }
+        System.out.println();
+        System.out.println(freg[marks[4]]);
 
     }
 }
