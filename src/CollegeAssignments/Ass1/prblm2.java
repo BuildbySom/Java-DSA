233fcddfa2d120c628a050b66024b76c6ce273fd
soumya <soumyasundardas2@gmail.com>
2026-01-15 19:21:35 +0530

    practice:a new problem solved

--- src/CollegeAssignments/Ass1/prblm2.java
+++ src/CollegeAssignments/Ass1/prblm2.java
@@ -4,23 +4,23 @@ public class prblm2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Find The Average of nos....");
-        boolean ch=false;
-        int[] arr = new int[1000];
-        int i=0;
-        while(ch==true){
-            System.out.println("Enter the nos:");
+        boolean ch=true;
+        int[] arr = new int[8];
+        System.out.println("Enter the nos:");
+        int sum=0;
+        for(int i=0;i<arr.length;i++) {
             arr[i] = sc.nextInt();
-            i++;
-            ch=false;
-
-
-
+            sum+=arr[i];
         }
-        for(int j=0;j<arr.length;j++){
+        System.out.println("Array Elements:");
+        for(int i=0;i<arr.length;i++) {
 
-            System.out.print(arr[j]+" ");
-
-
+            System.out.print(arr[i]+" ");
         }
+        System.out.println();
+        double avg=(double)sum/8;
+        System.out.println("Avg: "+avg);
     }
+
 }
+
---
+++ src/CollegeAssignments/test.java
@@ -0,0 +1,12 @@
+package CollegeAssignments;
+
+public class test {
+    public static void main(String[] args) {
+        int[] arr=new int[6];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+
+        System.out.println(arr[1]);
+    }
+}
