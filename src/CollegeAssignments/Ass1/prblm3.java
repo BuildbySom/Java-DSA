cbbeb18fad353f0971af1af6c101e4440c9e86e9
soumya <soumyasundardas2@gmail.com>
2026-02-14 16:56:43 +0530

    practice:2 problems solved

--- src/CollegeAssignments/Ass1/prblm3.java
+++ src/CollegeAssignments/Ass1/prblm3.java
@@ -1,4 +1,34 @@
 package CollegeAssignments.Ass1;
 
+import java.util.Scanner;
+
+//area & perimeter of  rectangle
+class Rectangle{
+    private double width,height;
+    public void area(double w,double h){
+        this.width=w;
+        this.height=h;
+        double area= w*h;
+        System.out.println("Area of Rectangle: "+area);
+    }
+    public void perimeter(double w,double h){
+        this.width=w;
+        this.height=h;
+        double perimeter= 2*(w+h);
+        System.out.println("Perimeter of Rectangle: "+perimeter);
+    }
+
+
+}
 public class prblm3 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the Width:");
+        double w=sc.nextDouble();
+        System.out.println("Enter the Hight:");
+        double h=sc.nextDouble();
+        Rectangle r1=new Rectangle();
+        r1.area(w,h);
+        r1.perimeter(w,h);
+    }
 }
--- src/CollegeAssignments/Ass1/problm4.java
+++ src/CollegeAssignments/Ass1/problm4.java
@@ -1,4 +1,18 @@
 package CollegeAssignments.Ass1;
 
+import java.util.Scanner;
+
+//leap year
 public class problm4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Year:");
+        int yr=sc.nextInt();
+        if(yr%4==0 || yr%400==0){
+            System.out.println(yr + " Is a leap year");
+        }
+        else{
+            System.out.println("Not a leap year...");
+        }
+    }
 }
