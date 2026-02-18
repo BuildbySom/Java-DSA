596bd2112355feb636ece44c028e514fa6dbcd37
soumya <soumyasundardas2@gmail.com>
2026-02-18 10:18:04 +0530

    practice:2 problems solved

--- src/CollegeAssignments/Ass1/prblm5.java
+++ src/CollegeAssignments/Ass1/prblm5.java
@@ -1,4 +1,41 @@
 package CollegeAssignments.Ass1;
 
-public class prblm5 {
+import java.util.Scanner;
+
+//Largest among 3 numbers:
+class max{
+    private int a,b,c;
+    public void greatest(int a,int b,int c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+        if(a==b && b==c && c==a){
+            System.out.println("Numbers are not Distinct..");
+        }
+        else{
+            if(a>b && a>c){
+                System.out.println(a+" is Greatest");
+            }
+            if(b>a && b>c){
+                System.out.println(b+" is the Greatest");
+            }
+            if(c>a && c>b){
+                System.out.println(c+" is the greatest");
+            }
+
+        }
+    }
 }
+public class prblm5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 3 nos: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        max m1 = new max();
+        m1.greatest(a,b,c);
+
+    }
+    }
+
--- src/CollegeAssignments/Ass1/prblm6.java
+++ src/CollegeAssignments/Ass1/prblm6.java
@@ -1,4 +1,55 @@
 package CollegeAssignments.Ass1;
 
+import java.util.Scanner;
+
 public class prblm6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter distance(in meters)");
+        int dist= sc.nextInt();
+        System.out.println("Enter The Time Taken :");
+        System.out.println("Hours: ");
+        int h= sc.nextInt();
+        System.out.println("Minutes: ");
+        int m=sc.nextInt();
+        System.out.println("Seconds: ");
+        int s= sc.nextInt();
+        // Convert total time into seconds
+        int totalSeconds = (h * 3600) + (m * 60) + s;
+
+        if (totalSeconds == 0) {
+            System.out.println("Time cannot be zero.");
+            return;
+        }
+
+        // Speed in meters per second
+        double speedMps = dist / totalSeconds;
+
+        // Speed in km per hour
+        double speedKmph = speedMps * 3.6;
+
+        // Speed in miles per hour
+        double speedMph = speedMps * 2.23694;
+
+        System.out.println("Speed in m/s: " + speedMps);
+        System.out.println("Speed in km/h: " + speedKmph);
+        System.out.println("Speed in mph: " + speedMph);
+
+        sc.close();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
