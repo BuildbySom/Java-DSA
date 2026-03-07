7d54aeba757a76d59cb5d32880f1db1bfec14027
soumya <soumyasundardas2@gmail.com>
2026-03-07 17:06:07 +0530

    practice:2 problems solved

--- src/CollegeAssignments/Ass2/prblm4.java
+++ src/CollegeAssignments/Ass2/prblm4.java
@@ -1,4 +1,7 @@
 package CollegeAssignments.Ass2;
-
+//prime npo till n
 public class prblm4 {
+    public static void main(String[] args) {
+
+    }
 }
--- src/CollegeAssignments/Ass2/prblm5.java
+++ src/CollegeAssignments/Ass2/prblm5.java
@@ -1,4 +1,12 @@
 package CollegeAssignments.Ass2;
 
 public class prblm5 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
 }
