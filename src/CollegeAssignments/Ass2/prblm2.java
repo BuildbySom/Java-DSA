5821560c1fb39312d7e4405fcd5df157dfb98a10
soumya <soumyasundardas2@gmail.com>
2026-03-07 17:00:36 +0530

    practice:2 problems solved

--- src/CollegeAssignments/Ass2/prblm2.java
+++ src/CollegeAssignments/Ass2/prblm2.java
@@ -1,4 +1,19 @@
 package CollegeAssignments.Ass2;
+import java.util.Scanner;
 
 public class prblm2 {
+    public static void factorial(int n){
+        int fac =1;
+        for(int i=1;i<=n;i++){
+            fac*=i;
+        }
+        System.out.println("Factorial: "+fac);
+    }
+    public static void main(String[] args) {
+        System.out.println("Calculating Factorial...");
+        System.out.println("Enter a number :");
+        Scanner sc = new Scanner(System.in);
+        int user_input= sc.nextInt();
+        factorial(user_input);
+    }
 }
--- src/CollegeAssignments/Ass2/prblm3.java
+++ src/CollegeAssignments/Ass2/prblm3.java
@@ -1,4 +1,26 @@
 package CollegeAssignments.Ass2;
 
+import java.util.Scanner;
+
+//fibonacci series till n
 public class prblm3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int user_input= sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int temp;
+        System.out.println(a);
+        System.out.println(b);
+        //System.out.println(temp);
+        for(int i = 0;i<user_input-2;i++){
+            temp = a+b;
+            System.out.println(temp);
+            a= b;
+            b= temp;
+
+        }
+
+    }
 }
